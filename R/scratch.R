f5a1c6ab46890c76a69414106149ff9c3f6591c4
blaserlab <bradley.blaser@osumc.edu>
2022-05-06 14:05:02 -0400

    updated scratch and lock file

---
+++ R/scratch.R
@@ -0,0 +1,5 @@
+system.file("data-raw/load_process.R", package = "baiocchi.long.datapkg")
+
+# navigate to renv/library/R-4.2/x86_64-pc-linux-gnu/baiocchi.long.datapkg/data-raw/load_process.R
+
+"~/network/X/Labs/Blaser/single_cell/baiocchi_long_20211209/analysis_config.csv"
