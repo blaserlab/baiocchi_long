1514f510c11a34cad0602ba95992760cdf713196
blaserlab <bradley.blaser@osumc.edu>
2022-11-02 12:40:44 -0400

    worked on fig 3 with mackenzie

--- R/manuscript_figures.R
+++ R/manuscript_figures.R
@@ -59,94 +59,7 @@ leiden_enrichment_barplot <- ggplot(leiden_long_short_enrichment_tbl,
   expand_limits(y = 6)
 
 
-# gene set umaps ----------------------------------------------------
-bb_gene_umap(cds_human_pass_sf, bb_rowmeta(cds_human_pass_sf) |> select(feature_id, IGF1_signaling) |> filter(IGF1_signaling), max_expr_val = 3.5)
-
-
-#figure_3G
-# # add gene sets provided by mackenzie
-# mac_genesets_to_add <- map2_dfr(
-#   .x = list.files(
-#     "~/network/X/Labs/Blaser/share/collaborators/baiocchi_long_manuscript/genesets/xls3",
-#     full.names = TRUE
-#   ),
-#   .y = c(
-#     "mtor",
-#     "igf",
-#     "regulation_of_eif4_and_p70S6k",
-#     "estrogen_receptor"
-#   ),
-#   .f = \(x, y, cds = cds_human_pass_sf) {
-#     readxl::read_excel(x) |>
-#       left_join(bb_rowmeta(cds),
-#                 by = c("Symbol" = "gene_short_name")) |>
-#       filter(!is.na(feature_id)) |>
-#       select(feature_id) |>
-#       mutate(mac_geneset = TRUE) |>
-#       right_join(bb_rowmeta(cds)) |>
-#       select(feature_id, mac_geneset) |>
-#       pivot_longer(mac_geneset) |>
-#       mutate(value = ifelse(is.na(value), FALSE, TRUE)) |>
-#       mutate(name = y)
-#
-#
-#   }
-# ) |> pivot_wider(names_from = "name", values_from = "value")
-#
-# cds_human_pass_sf <- bb_tbl_to_rowdata(obj = cds_human_pass_sf, min_tbl = mac_genesets_to_add)
-#
-# dir.create("data")
-#
-# save(cds_human_pass_sf, file = "data/cds_human_pass_sf.rda", compress = "bzip2")
-#
-# bb_rowmeta(cds_human_pass_sf)
-#
-# # produce the plots
-# walk2(
-#   .x = c(
-#     "mtor",
-#     "igf",
-#     "regulation_of_eif4_and_p70S6k",
-#     "estrogen_receptor"
-#
-#   ),
-#   .y = c(Inf, 4, 6, 10, Inf, 3.5, Inf, 2.5, 4, 3),
-#   .f = \(x, y, dat = cds_human_pass_sf, figdir = figs_out) {
-#     p <-
-#       bb_gene_umap(
-#         dat,
-#         bb_rowmeta(dat) |> filter(!!sym(x) &
-#                                     !is.na(module))
-#         |> select(feature_id, !!sym(x)),
-#         max_expr_val = y
-#       ) +
-#       theme(strip.text = element_blank()) +
-#       labs(title = x)
-#     p <- ggrastr::rasterise(p, dpi = 300)
-#     save_plot(
-#       filename = str_glue("{figdir}/mac_geneset_figs/{x}.pdf"),
-#       plot = p,
-#       base_width = 5.4,
-#       base_height = 4.2
-#     )
-#   }
-# )
-#
-# # print out the gene sets used
-# bb_rowmeta(cds_human_pass_sf) |>
-#   select(
-#     gene_short_name,
-#     mtor,
-#     igf,
-#     regulation_of_eif4_and_p70S6k,
-#     estrogen_receptor
-#   ) |>
-#   pivot_longer(-gene_short_name, names_to = "pathway") |>
-#   filter(value) |>
-#   select(gene_short_name, pathway) |>
-#   arrange(pathway) |>
-#   write_csv(str_glue("{figs_out}/mac_geneset_figs/pathways.csv"))
-
+# gene set heat map -----------------------------------------------
 
 agg_mat_list <- map(
   .x = c(
@@ -157,50 +70,50 @@ agg_mat_list <- map(
 
   ),
   .f = \(x, dat = cds_human_pass_sf) {
-    mat <- aggregate_gene_expression(
+    mat <- bb_aggregate(
       dat,
       gene_group_df = bb_rowmeta(dat) |> select(feature_id, !!sym(x)),
       cell_group_df = bb_cellmeta(dat) |> select(cell_id, leiden),
       scale_agg_values = F
     ) |>
       t()
-    mat <- matrix(mat[, "TRUE"])
-    colnames(mat) <- x
-    return(mat)
+    true_mat <- matrix(mat[, "TRUE"])
+    colnames(true_mat) <- x
+    rownames(true_mat) <- rownames(mat)
+    return(true_mat)
 
   }
 )
 
 agg_mat <- bind_cols(agg_mat_list) |>
   scale()
-rownames(agg_mat) <- paste0("leiden ", 1:13)
+rownames(agg_mat) <- paste0(1:13)
 
-heatmap_rowanno_df <- bb_cellmeta(cds_human_pass_sf) |>
-  group_by(leiden, partition) |>
-  summarise() |>
-  mutate(leiden = paste0("leiden ", leiden)) |>
-  column_to_rownames("leiden")
+# heatmap_rowanno_df <- bb_cellmeta(cds_human_pass_sf) |>
+#   group_by(leiden, partition) |>
+#   summarise() |>
+#   mutate(leiden = paste0("leiden ", leiden)) |>
+#   column_to_rownames("leiden")
 
 heatmap_colfun <-
   circlize::colorRamp2(breaks =  c(min(agg_mat), 0, max(agg_mat)), colors = heatmap_3_colors)
 
 pathway_heatmap <- grid.grabExpr(draw(
   Heatmap(
-    agg_mat,
+    t(agg_mat),
     name = "Pathway\nExpression",
     column_names_rot = 30,
+    column_dend_height = unit(2, "mm"),
+    row_dend_width = unit(2, "mm"),
+    row_names_gp = gpar(fontsize = 9),
+    column_names_gp = gpar(fontsize = 9),
     # right_annotation = heatmap_rowanno,
     col = heatmap_colfun,
-    column_title = "Pathway",
-    row_title = "Cell Cluster"
-  )
+    heatmap_legend_param = list(
+      direction = "horizontal",
+      title_position = "lefttop")
+  ), heatmap_legend_side = "bottom"
 ), wrap = T)
 
 plot_grid(pathway_heatmap)
 
-save_plot(
-  pathway_heatmap,
-  filename = str_glue("{figs_out}/mac_geneset_figs/pathway_heatmap.pdf"),
-  base_height = 5,
-  base_width = 5
-)
