01aeba3bc2526a6cac0315dfe06e09e0c7256bc7
blaserlab <bradley.blaser@osumc.edu>
2023-03-28 14:50:09 -0400

    made pathway figs in bwb_figs_20230328.R

---
+++ R/bwb_figs_20230328.R
@@ -0,0 +1,25 @@
+bb_rowmeta(cds_human_pass_sf) |> glimpse()
+bb_gene_umap(cds_human_pass_sf,
+             bb_rowmeta(cds_human_pass_sf) |>
+               filter(mtor) |>
+               mutate(label = "MTOR Pathway") |>
+               select(feature_id, label), max_expr_val = 4)
+
+bb_gene_umap(cds_human_pass_sf,
+             bb_rowmeta(cds_human_pass_sf) |>
+               filter(IGF1_signaling) |>
+               mutate(label = "IGF1 Signaling") |>
+               select(feature_id, label), max_expr_val = 3)
+
+bb_gene_umap(cds_human_pass_sf,
+             bb_rowmeta(cds_human_pass_sf) |>
+               filter(eif4_p70S6k) |>
+               mutate(label = "P70S6K Signaling") |>
+               select(feature_id, label))
+
+bb_gene_umap(cds_human_pass_sf,
+             bb_rowmeta(cds_human_pass_sf) |>
+               filter(ERK_MAPK_signaling) |>
+               mutate(label = "MEK/ERK Signaling") |>
+               select(feature_id, label), max_expr_val = 3.5)
+
